e7973021ef8d968158069e5fd8762336efd3630e
Alejandro Chavez Barrera <CrazyScienceLex@gmail.com>
2023-12-29 06:04:51 -0600

    Modificaciones: "lib.rs, main.rs" codigo para hacer split, indagaciones personales en forma de comentarios

--- split_string_lex/src/lib.rs
+++ split_string_lex/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//crates and traits zone
+use std::io::{BufReader, BufRead};
+use clap::Parser;
+
+//atributtes zone:
+#[derive(Parser)]
+#[command(name = "resplit", version = "0.0.1", author = "Alejandro Chavez Barrera, Alfredo Deza", about = "Split strings by one or more dilimeters and return a field like cut" long_about = None)]
+//This struct is an attribute too, that uses the clap crate to parse the command line arguments, and also uses the attributes above this comment (i know it uses parser but i'm not sure in what way: ¿as entry?, ¿as function that passes the entry?)
+pub struct Cli {
+    #[arg(short('f'))]
+    field: usize,
+    #[arg(short('d'))]
+    delimeter: String,
+    #[arg(short('s'))]
+    debug: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn read_stdin() -> String { //esta funcion: lee una entrada directamente del teclado y la devuelve como un string.
+    let stdin = std::io::stdin();
+    let mut reader = BufReader::new(stdin.lock());
+    let mut line = String::new();
+    reader.read_line(&mut line).expect("Failed to read input line");
+    line.trim().to_string()
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+
+pub fn split(s: String, cli: &Cli) -> String { // esta funcion: lee una entrada proveniente de la linea de comandos (¿imagino que el resultado de la operacion read_stdin?) y provee como salida: el resultado de separar la string de entrada, este resultado se encuentra en forma de: ¿vector, arreglo de objetos, lista (debido a los indices que puedo ver)?
+    let parts: Vec<&str> = s.split(&cli.delimeter).collect();
+    if cli.debug {
+        println!("Parts: {:?}", parts);
+        println!("Indexes available starting at 0: {}", parts.len());
     }
+    parts.get(cli.field).unwrap_or(&"").to_string()
 }
--- split_string_lex/src/main.rs
+++ split_string_lex/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//Llamada a nuestras "librerias externas" ==> crates
+    //crates son: resplit,clap
+//Llamada de: "librerias de apoyo que expanden las funcionalidades de una crates/ librerias simbioticas" == "traits"
+    //traits son: Cli, Parser
+
+use resplit::Cli;
+use clap::Parser;
+
+fn main() { //una funcion que se llama main es el punto de entrada de nuestra aplicacion, lo que entiendo que hace en este momento es:
+    let cli = Cli::parse(); //Realiza un parse de los argumentos de la linea de comandos.
+    println!("{}", cli.input); //Imprime el argumento de la linea de comandos actual.
+    println!("{}", cli.output); //Imprime el argumento de la linea de comandos actual ///en caso de que exista algun argumento de salida (en este caso el argumento de salida es: ¿resultado?)
+    let buffer = resplit::read_stdin(); //lee el contenido del archivo de entrada
+
+    let result = resplit::split(buffer, &cli); //realiza el split del contenido del archivo de entrada: "&cli", tomando en cuenta el "caracter o palabra que indica cuando debe realizarse un split de inmediato: buffer. 
+    //Pero mas alla del nombre buffer cu ¿cual es esa palabra,caracter,delimitante?"
+    println!("el resultado es: {}",result); // imprime el resultado del split realizado al leer el contenido del archivo de entrada.
 }
